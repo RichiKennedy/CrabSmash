aecb08d597398373db794e4e580677512106cbe5
RichiKennedy <richard.kennedy@hyperisland.se>
2022-05-27 10:56:08 +0200

    javascript functionality and some sound effects

--- main.js
+++ main.js
@@ -1,5 +1,43 @@
 const cursor = document.querySelector(".cursor");
+const holes = document.querySelectorAll(".hole");
+const sound = new Audio("assets/karateChop.mp3");
+const scoreElement = document.querySelector(".score span");
+let score = 0;
 
+function run() {
+  const index = Math.floor(Math.random() * holes.length);
+  const hole = holes[index];
+  let timer = null;
+
+  const image = document.createElement("img");
+  image.classList.add("crab");
+  image.src = "assets/crab.svg";
+
+  image.addEventListener("click", () => {
+    sound.play();
+    score += 10;
+    scoreElement.textContent = score;
+    image.src = "assets/HitCrab.svg";
+    clearTimeout(timer);
+    setTimeout(() => {
+      hole.removeChild(image);
+      run();
+    }, 500);
+  });
+
+  if (score === 100) {
+    alert("newlevel");
+  } else hole.appendChild(image);
+
+  timer = setTimeout(() => {
+    hole.removeChild(image);
+
+    run();
+  }, 1500);
+}
+run();
+
+// Make the hand follow cursor
 window.addEventListener("mousemove", (e) => {
   cursor.style.top = e.pageY + "px";
   cursor.style.left = e.pageX + "px";
